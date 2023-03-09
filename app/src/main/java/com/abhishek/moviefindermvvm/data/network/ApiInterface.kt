4703ac11df4a9f95f87f0acc42600362a567c46b
cheetahmail007 <cheetahmail007@gmail.com>
2023-03-10 04:03:10 +0530

    Updated Constant

--- app/src/main/java/com/abhishek/moviefindermvvm/data/network/ApiInterface.kt
+++ app/src/main/java/com/abhishek/moviefindermvvm/data/network/ApiInterface.kt
@@ -2,6 +2,7 @@ package com.abhishek.moviefindermvvm.data.network
 
 import com.abhishek.moviefindermvvm.data.model.MovieDetail
 import com.abhishek.moviefindermvvm.data.model.SearchResults
+import com.abhishek.moviefindermvvm.data.network.Constant.BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
@@ -31,13 +32,13 @@ interface ApiInterface {
             val okkHttpclient = OkHttpClient.Builder()
                 .addInterceptor(networkConnectionInterceptor)
                 .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-                .connectTimeout(1, TimeUnit.MINUTES)
+                .connectTimeout(30, TimeUnit.MINUTES)
                 .readTimeout(30, TimeUnit.SECONDS)
                 .build()
 
             return Retrofit.Builder()
                 .client(okkHttpclient)
-                .baseUrl("https://www.omdbapi.com/")
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(ApiInterface::class.java)
---
+++ app/src/main/java/com/abhishek/moviefindermvvm/data/network/Constant.kt
@@ -0,0 +1,5 @@
+package com.abhishek.moviefindermvvm.data.network
+
+object Constant {
+    const val BASE_URL = "https://www.omdbapi.com/"
+}
